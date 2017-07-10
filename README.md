31463836df3c06bf24e0e1894438b1b55eb28f7f
s-vde <sanne-acc@posteo.net>
2017-07-10 17:05:33 +0200

    [build] simplify the build process - add cpp-utils as a submodule - remove unnecessary variables from cmake - remove unnecessary variables from ./instrument.sh - update README

--- README.md
+++ README.md
@@ -1,24 +1,52 @@
-# record-replay
+# Record-Replay
 LLVM Instrumentation Pass and Scheduler for recording the execution of multithreaded programs and
 replaying them under a given thread interleaving.
 
-### Dependencies
-Both the Instrumentation Pass and the Scheduler use c++ utility functions found in the repository
-[cpp-utils](https://github.com/s-vde/cpp-utils).
+---
 
-### Build LLVM
+## Cloning and Building Record-Replay
+
+#### Cloning the Repository
+
+Record-Replay uses [cpp-utils](https://github.com/s-vde/cpp-utils) as a submodule. Therefore, clone recursively:
+
+```
+git clone --recursive https://github.com/s-vde/record-replay.git
+```
+
+#### Build LLVM
 The Instrumentation Pass needs to be built against the LLVM Pass Framework.
 It is currently tested with LLVM and Clang version 4.0.
 Scripts for downloading/cloning and building the LLVM Suite, the Clang frontend
 and required tools with the right configuration can be found in the repository
 [llvm-pass-development-scripts](https://github.com/s-vde/llvm-pass-development-scripts).
 
-### Instrumenting a program
+#### Build Record-Replay
+
+Once you have a build of the LLVM Pass Framework, Record-Replay can be built using cmake. By default the build process will look for and use an installed LLVM. A custom built LLVM can be specified as follows:
+
+```
+cmake -DLLVM_DIR=<path_to_llvm_build_dir>/lib/cmake/llvm 
+```
+
+---
+
+## Instrumenting a Program
 In order to instrument an input program, run
 
-`source <path_to_record_replay>/instrument.sh <abs_path_to_record_replay> <path_to_llvm> <path_to_cpp_utils> <path_to_input_program> <path_to_output_dir>`
+```
+./instrument.sh
+     <record_replay_build_dir>
+     <path_to_llvm> 
+     <path_to_input_program> 
+     <path_to_output_dir>
+     (compiler_options)
+```
+where all path variables can be either relative or absolute to the current directory.
 
-### Running the instrumented program
+---
+
+## Running the Instrumented Program
 When the instrumented program `<output_dir>/<program_name>` is run, it expects the following files (relative to the place from where it is run):
 - `schedules/schedule.txt`: containing the schedule under which the program is to be run (e.g. `<0,0,1,1>`)
 - `schedules/threads.txt`: containing the number of threads in the program
--- instrument.sh
+++ instrument.sh
@@ -1,59 +1,51 @@
 #!/bin/bash
 
+####################
+# PARAMETERS
+
+record_replay_build=`echo $1`
+llvm_base=`echo $2`
+input_program=`echo $3`
+nr_threads=`echo $4`
+output_dir=`echo $5`
+compiler_options=`echo $6`
+
+
+####################
+# VARIABLES
+
 here=`pwd .`
 
-record_replay_base=`echo $1`
-
-pass_build=${record_replay_base}/build/src/llvm-pass/LLVMRecordReplayPass.dylib
+pass_build=${record_replay_build}/src/llvm-pass/LLVMRecordReplayPass.dylib
 pass_name="-instrument-record-replay-lw"
-scheduler_build=${record_replay_base}/build/src/scheduler/libRecordReplayScheduler.dylib
+scheduler_build=${record_replay_build}/src/scheduler
 
-llvm_base=`echo $2`
 llvm_bin=${llvm_base}/build/bin
 
-cpp_utils_base=`echo $3`
-
-input_program=`echo $4`
 input_filename=$(basename "${input_program}")
 input_extension="${input_filename##*.}"
 
-nr_threads=`echo $5`
-
-output_dir=`echo $6`
-
-custom_strategies=`echo $7`
-if [ -z "${custom_strategies// }" ]; then
-   custom_strategies=${record_replay_base}/src/scheduler/strategies/custom
-fi
-
-###
-echo Instrumenting ${input_program}
-###
-
-compiler_flags="-O0 -g -DNR_THREADS=${nr_threads} -pthread -emit-llvm "
+compiler_options+=" -O0 -g -DNR_THREADS=${nr_threads} -pthread -emit-llvm "
 
 if [ ${input_extension} == "c" ]; then
    compiler=${llvm_bin}/clang
 else
    compiler=${llvm_bin}/clang++
-   compiler_flags+="-std=c++14 "
 fi
 
-test -d ${record_replay_base}/build || mkdir ${record_replay_base}/build
-cd ${record_replay_base}/build/
-cmake ../ \
-   -DLLVM_DIR=${llvm_base}/build/lib/cmake/llvm \
-   -DPROGRAM_MODEL_SRC=${record_replay_base}/src/program-model \
-   -DCUSTOM_STRATEGIES=${custom_strategies} \
-   -DUTILS_SRC=${cpp_utils_base}/src
-make
-cd ${here}
+
+####################
+# INSTRUMENTATION
+
+echo Instrumenting ${input_program}
 
 test -d ${output_dir} || mkdir -p ${output_dir}
-${compiler} ${compiler_flags} -c ${input_program} -o ${output_dir}/${input_filename}.bc
+${compiler} ${compiler_options} -c ${input_program} -o ${output_dir}/${input_filename}.bc
+
 echo [record-replay] run instrumentation pass
 ${llvm_bin}/opt -S -load ${pass_build} ${pass_name} < ${output_dir}/${input_filename}.bc > ${output_dir}/${input_filename}.instrumented.bc
+
 echo [record-replay] link with scheduler library
 ${compiler} ${output_dir}/${input_filename}.instrumented.bc \
-   ${scheduler_build} -rpath ${record_replay_base}/build/src/scheduler/ \
+   ${scheduler_build}/libRecordReplayScheduler.dylib -rpath ${scheduler_build} \
    -o ${output_dir}/"${input_filename%.*}"
