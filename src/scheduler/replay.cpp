58f53c3b3cf32100375dc9a076569fe8179eee64
s-vde <sanne-acc@posteo.net>
2017-09-11 14:13:15 +0200

    [format] scheduler/replay

--- src/scheduler/replay.cpp
+++ src/scheduler/replay.cpp
@@ -1,62 +1,57 @@
 
 #include "replay.hpp"
 
-// SCHEDULER
+#include "program.hpp"
 #include "scheduler_settings.hpp"
 
-// PROGRAM_MODEL
-#include "program.hpp"
+#include <container_output.hpp>
 
-// UTILS
-#include "container_output.hpp"
-
-// STL
 #include <fstream>
 #include <sys/stat.h>
 
-namespace scheduler
+
+namespace scheduler {
+
+//--------------------------------------------------------------------------------------------------
+
+void run_under_schedule(const Program& program, const schedule_t& schedule)
 {
-   //-------------------------------------------------------------------------------------
-   
-   void run_under_schedule(const Program& B, const schedule_t& S)
-   {
-      write_schedules(B, S);
-      system(B.to_string().c_str());
-   }
-   
-   //-------------------------------------------------------------------------------------
-    
-   void run_under_schedule(const Program& B,
-                           const schedule_t& S,
-                           const SchedulerSettings& settings)
-   {
-      write_settings(settings);
-      run_under_schedule(B, S);
-   }
-   
-   //-------------------------------------------------------------------------------------
-    
-   void write_settings(const SchedulerSettings& settings)
-   {
-      system("test -d schedules || mkdir schedules");
-      std::ofstream ofs("schedules/settings.txt");
-      ofs << settings;
-      ofs.close();
-   }
-   
-   //-------------------------------------------------------------------------------------
-    
-   void write_schedules(const Program& B, const schedule_t& S)
-   {
-      system("test -d schedules || mkdir schedules");
-      std::ofstream ofs("schedules/threads.txt");
-      ofs << B.nr_threads();
-      ofs.close();
-      ofs.open("schedules/schedule.txt");
-      ofs << S;
-      ofs.close();
-   }
-   
-   //-------------------------------------------------------------------------------------
-   
+   write_schedules(program, schedule);
+   system(program.to_string().c_str());
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void run_under_schedule(const Program& program, const schedule_t& schedule,
+                        const SchedulerSettings& settings)
+{
+   write_settings(settings);
+   run_under_schedule(program, schedule);
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void write_settings(const SchedulerSettings& settings)
+{
+   system("test -d schedules || mkdir schedules");
+   std::ofstream ofs("schedules/settings.txt");
+   ofs << settings;
+   ofs.close();
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void write_schedules(const Program& program, const schedule_t& schedule)
+{
+   system("test -d schedules || mkdir schedules");
+   std::ofstream ofs("schedules/threads.txt");
+   ofs << program.nr_threads();
+   ofs.close();
+   ofs.open("schedules/schedule.txt");
+   ofs << schedule;
+   ofs.close();
+}
+
+//--------------------------------------------------------------------------------------------------
+
 } // end namespace scheduler
--- src/scheduler/replay.hpp
+++ src/scheduler/replay.hpp
@@ -1,43 +1,41 @@
 #pragma once
 
-// SCHEDULER
 #include "schedule.hpp"
 
-// STL
 #include <string>
 
-//--------------------------------------------------------------------------------------90
+//--------------------------------------------------------------------------------------------------
 /// @file replay.hpp
-/// @brief Free functions providing an interface to the replay functionality of the
-/// Scheduler.
+/// @brief Free functions providing an interface to the replay functionality of the Scheduler.
 /// @author Susanne van den Elsen
 /// @date 2015
-//----------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------
 
-namespace scheduler
-{
-   //-------------------------------------------------------------------------------------
-   
-	// forward declarations
-   class Program;
-   class SchedulerSettings;
-	
-   //-------------------------------------------------------------------------------------
-   
-   /// @brief Run the program under the settings currently in schedules/settings.txt.
-   /// @note If schedules/settings.txt does not exist, the Scheduler may not behave as
-   /// expected.
 
-   void run_under_schedule(const Program&, const schedule_t&);
-    
-   void run_under_schedule(const Program&, const schedule_t&, const SchedulerSettings&);
-   
-   //-------------------------------------------------------------------------------------
-    
-   void write_settings(const SchedulerSettings&);
-    
-   void write_schedules(const Program&, const schedule_t&);
-   
-   //-------------------------------------------------------------------------------------
-    
+namespace scheduler {
+
+// forward declarations
+class Program;
+class SchedulerSettings;
+
+
+//--------------------------------------------------------------------------------------------------
+
+/// @brief Run the program under the settings currently in schedules/settings.txt.
+/// @note If schedules/settings.txt does not exist, the Scheduler may not behave as
+/// expected.
+
+void run_under_schedule(const Program&, const schedule_t&);
+
+void run_under_schedule(const Program&, const schedule_t&, const SchedulerSettings&);
+
+//--------------------------------------------------------------------------------------------------
+
+
+void write_settings(const SchedulerSettings&);
+
+void write_schedules(const Program&, const schedule_t&);
+
+//--------------------------------------------------------------------------------------------------
+
 } // end namespace scheduler
